11cf20fc68917a7517355159dc17742c773d5d8c
bswalter8 <bswalter8@gmail.com>
2022-03-07 00:12:06 -0300

    second commit

--- entrega4c.js
+++ entrega4c.js
@@ -1,41 +1,29 @@
 
-
 const Biblioteca = {
     nombre_b: "Bilioteca de Babel",
     sd: "Centro",
     email : "babelbilioteca@babel.org"
  }
 
-
-
 class Libro {   
     constructor(nombre, autor){
         this.nombre = nombre;
         this.autor = autor;    
-    }
-    
+    }    
 }
-
 
 const {
     nombre_b : nombre_Biblioteca,
     sd : sede,
   
  } = Biblioteca
-console.log(nombre_Biblioteca);
 
 let formulario = document.getElementById("idForm");
 let catalogo_Mostrar = document.getElementById("catalogo_Mostrar");
 let titulo = document.getElementById("h1");
-
 titulo.innerHTML =  `<p>Agregado a la base de datos - ${nombre_Biblioteca} - ${sede}</p>`;
-
-
 let catalogo = [];
-
-
 localStorage.getItem("catalogo") ? catalogo = JSON.parse(localStorage.getItem("catalogo")) : localStorage.setItem("catalogo", JSON.stringify(catalogo));
-
 
 formulario.addEventListener("submit", e =>{
     e.preventDefault();
@@ -45,23 +33,17 @@ formulario.addEventListener("submit", e =>{
     let nombre = dataForm.get("nombre");
     let autor = dataForm.get("autor");
     let alerta = document.getElementById("alerta");
-    nombre == "" || autor == "" ? alerta.innerHTML = "<p>Debe ingresar ambos parametros</p>": n_Libro = new Libro(nombre, autor);
-  
+    nombre == "" || autor == "" ? alerta.innerHTML = "<p>Debe ingresar ambos parametros</p>": n_Libro = new Libro(nombre, autor); 
     if (n_Libro){
-
         const guardar_Catalogo = {
             ...n_Libro,
             ...Biblioteca,
         }
-
         catalogo.push(guardar_Catalogo);
         localStorage.setItem("catalogo", JSON.stringify(catalogo));
         alerta.innerHTML = `<p>"${guardar_Catalogo.nombre}" Agregado a la base de datos</p>`;
     }
-  
-
 });
-
 
 formulario.addEventListener('keypress', e =>{
     alerta.innerHTML = ""
